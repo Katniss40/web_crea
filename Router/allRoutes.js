5a9c03f87a13f8ce12820dbc460bf8d9a68be9a6
Bourdeloux Corinne <bourdeloux.corinne@orange.fr>
2025-01-05 23:08:55 +0100

    mise en page de "HOME"

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -1,8 +1,18 @@
 import Route from "./Route.js";
 
+//constructor(url, title, pathHtml, pathJS = "")
+
 //Définir ici vos routes
 export const allRoutes = [
-    new Route("/", "Accueil", "/pages/home.html"),];
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/administrative", "administrative", "/pages/administrative.html"),
+    new Route("/informatique", "informatique", "/pages/informatique.html"),
+    new Route("/tarifs", "tarifs", "/pages/tarifs.html"),
+    new Route("/confiance", "confiance", "/pages/confiance.html"),
+    new Route("/connexion", "connexion", "/pages/connexion.html"),
+    new Route("/rdv", "rdv", "/pages/rendez_vous.html"),
+    
+];
     
 //Le titre s'affiche comme ceci : Route.titre - websitename
 export const websiteName = "Web Crea 2.0";
